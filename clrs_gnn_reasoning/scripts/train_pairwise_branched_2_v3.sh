7fc387e414515fe18f271b23eaca2fea66975b78
lidongyue12138 <lidongyue12138@gmail.com>
2025-01-03 16:55:17 -0500

    update scripts

---
+++ clrs_gnn_reasoning/scripts/train_pairwise_branched_2_v3.sh
@@ -0,0 +1,19 @@
+task_names=('bfs' "dfs" "topological_sort" "articulation_points" "bridges" "strongly_connected_components" "mst_kruskal" "mst_prim" "dijkstra" "bellman_ford" 'dag_shortest_paths' "floyd_warshall")
+
+length=${#task_names[@]}
+
+for ((i = 8; i < $length; i++)); do
+  for ((j = i + 1; j < $length; j++)); do
+    python train_mtl.py --algorithms "${task_names[$i]}" "${task_names[$j]}" \
+    --cfg "./configs/SAGE.yml" --lr 5e-5 --hidden_dim 128 --gnn_layers 3 --enable_gru --devices 1 --batch_size 8 --epochs 50 --runs 1 --loss_weight_hint 2\
+    --save_name "pairwise_branched_2" --train_branched_network --branch_layer 2
+  done
+done
+
+# python train_mtl.py --algorithms 'bfs' "dfs" \
+#   --cfg "./configs/SAGE.yml" --lr 5e-5 --hidden_dim 128 --gnn_layers 3 --enable_gru --devices 2 --batch_size 8 --epochs 10 --runs 1 --loss_weight_hint 2\
+#   --save_name "pairwise"
+
+# python train_mtl.py --algorithms 'bfs' "dfs" \
+#     --cfg "./configs/SAGE.yml" --lr 5e-5 --hidden_dim 128 --gnn_layers 3 --enable_gru --devices 2 --batch_size 8 --epochs 50 --runs 1 --loss_weight_hint 2\
+#     --save_name "pairwise_branched_1" --train_branched_network --branch_layer 1
---
+++ clrs_gnn_reasoning/scripts/tree_search_procedure/train_tree_structure.sh
@@ -0,0 +1,3 @@
+python train_mtl.py --algorithms 'bfs' "dfs" "topological_sort" "articulation_points" "bridges" "strongly_connected_components" "mst_kruskal" "mst_prim" "dijkstra" "bellman_ford" 'dag_shortest_paths' "floyd_warshall"\
+  --cfg "./configs/SAGE.yml" --lr 5e-5 --hidden_dim 128 --gnn_layers 3 --enable_gru --devices 1 --batch_size 8 --epochs 100 --runs 2 --loss_weight_hint 2\
+  --save_name "tree" --train_branched_network --tree_config_dir "tree_structure_1.txt"
