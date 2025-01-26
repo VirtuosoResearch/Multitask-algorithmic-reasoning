0efb224fe7cbce4748ce0ab6e206d1d002617d4e
lidongyue12138 <lidongyue12138@gmail.com>
2025-01-26 17:37:30 -0500

    update

--- graph-algorithms/scripts/evaluate_clrs_text_tasks/evaluate_few_shot_learning_v3.sh
+++ graph-algorithms/scripts/evaluate_clrs_text_tasks/evaluate_few_shot_learning_v3.sh
@@ -5,7 +5,7 @@ for shot in 40 100 200 400
 do
 python train_clrs_text.py --task_names $task \
     --model_key "meta-llama/Llama-3.1-8B" \
-    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 2000)) --max_output_length 150 --generate_output --runs 1 --lr 5e-5 \
+    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 200)) --max_output_length 150 --generate_output --runs 1 --lr 5e-5 \
     --save_name prompt_20 --epochs 0 --precision "bf16-true" --train_lora --lora_rank 16 --lora_alpha 128 --use_qlora \
     --few_shot_k $shot --only_evaluate_test_set
 done
@@ -15,7 +15,7 @@ for shot in 40 100 200 400
 do
 python train_clrs_text.py --task_names "strongly_connected_components" \
     --model_key "meta-llama/Llama-3.1-8B" \
-    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 2000)) --max_output_length 210 --generate_output --runs 1 --lr 5e-5 \
+    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 280)) --max_output_length 210 --generate_output --runs 1 --lr 5e-5 \
     --save_name prompt_20 --epochs 0 --precision "bf16-true" --train_lora --lora_rank 16 --lora_alpha 128 --use_qlora \
     --few_shot_k $shot --only_evaluate_test_set
 done
@@ -24,7 +24,7 @@ for shot in 40 100 200 400
 do
 python train_clrs_text.py --task_names "bridges" \
     --model_key "meta-llama/Llama-3.1-8B" \
-    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 5300)) --max_output_length 510 --generate_output --runs 1 --lr 5e-5 \
+    --devices 0 --batch_size 1 --inference_batch_size 1 --max_length $((shot * 530)) --max_output_length 510 --generate_output --runs 1 --lr 5e-5 \
     --save_name prompt_20 --epochs 0 --precision "bf16-true" --train_lora --lora_rank 16 --lora_alpha 128 --use_qlora \
     --few_shot_k $shot --only_evaluate_test_set
 done
