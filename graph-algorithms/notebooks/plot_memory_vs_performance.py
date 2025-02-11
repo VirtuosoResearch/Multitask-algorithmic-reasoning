a4dc42d28b17d991865f809445e1b0bcebbab540
lidongyue12138 <lidongyue12138@gmail.com>
2025-02-11 00:06:29 -0500

    update

---
+++ graph-algorithms/notebooks/plot_memory_vs_performance.py
@@ -0,0 +1,152 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+from matplotlib import rc
+rc('font', **{'family':'sans-serif','sans-serif':['Helvetica']})
+mpl.rcParams['savefig.dpi'] = 1200
+mpl.rcParams['text.usetex'] = True  # not really needed
+
+
+# Sample data points
+x = [
+126.171875,
+30.61132813,
+79.892578,
+104.7425781,
+66.171875,
+49.246875,
+]
+
+y = [
+86.0,
+83.6,
+84.9,
+85.0,
+86.4,
+87.3,
+]
+x = np.array(x) # convert to numpy array
+y = 100 - np.array(y) # compute error rate
+
+labels = [
+          r'$\mathrm{STN}$', 
+          r'$\mathrm{MTN}$', 
+          r'$\mathrm{MMoE}$', 
+          r'$\mathrm{TAG}$',
+          r'$\mathrm{LearningToBranch}$',
+          r'$\mathrm{AutoBRANE}$',]
+
+# Create scatter plot
+f, ax = plt.subplots(figsize=(8, 6))
+ax.scatter(x[1:-1], y[1:-1], color='orange', s=600)
+ax.scatter(x[-1], y[-1], color='forestgreen', s=600, marker='D')
+# Label each point
+for i, label in enumerate(labels):
+    if i in [5]:
+        ax.text(x[i]+9, y[i]-0.3, label, fontsize=32, ha='left', va='bottom')
+    elif i in [4]:
+        ax.text(x[i]+6, y[i]+0.2, label, fontsize=32, ha='left', va='top')
+    elif i in [3]:
+        ax.text(x[i]+6, y[i]-0.1, label, fontsize=32, ha='left', va='top')
+    elif i in [2]:
+        ax.text(x[i]+5, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    elif i in [1]:
+        ax.text(x[i]+3, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    # else:
+    #     ax.text(x[i]+7, y[i], label, fontsize=32, ha='left', va='top')
+
+# set x-axis to log scale
+# plt.xscale('log')
+plt.ylim(12.3, 17.5)
+plt.yticks([13, 14, 15, 16, 17])
+plt.xticks([0, 25, 50, 75, 100, 125])
+plt.xlim(-10, 160)
+
+# plt.title('Scatter Plot with Labels')
+plt.xlabel(r'$\mathrm{GPU~Memory~(GB)}$', fontsize=40)
+plt.ylabel(r'$\mathrm{Error~\%}$', fontsize=40)
+ax.tick_params(labelsize=40)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig("./figures/plot_memory_vs_performance.pdf", format="pdf", dpi=1200)
+plt.show()
+
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+from matplotlib import rc
+rc('font', **{'family':'sans-serif','sans-serif':['Helvetica']})
+mpl.rcParams['savefig.dpi'] = 1200
+mpl.rcParams['text.usetex'] = True  # not really needed
+
+
+# Sample data points
+x = [
+49.67396654,
+11.703125,
+38.17344599,
+38.79354745,
+26.86637231,
+18.94110577,
+]
+
+y = [
+75.2,
+71.6,
+71.7,
+73.1,
+73.7,
+74.6,
+]
+x = np.array(x) # convert to numpy array
+y = 100 - np.array(y) # compute error rate
+
+labels = [
+          r'$\mathrm{STN}$', 
+          r'$\mathrm{MTN}$', 
+          r'$\mathrm{MMoE}$', 
+          r'$\mathrm{TAG}$',
+          r'$\mathrm{LearningToBranch}$',
+          r'$\mathrm{AutoBRANE}$',]
+
+# Create scatter plot
+f, ax = plt.subplots(figsize=(8, 6))
+ax.scatter(x[1:-1], y[1:-1], color='orange', s=600)
+ax.scatter(x[-1], y[-1], color='forestgreen', s=600, marker='D')
+# Label each point
+for i, label in enumerate(labels):
+    if i in [5]:
+        ax.text(x[i]+4, y[i]-0.4, label, fontsize=32, ha='left', va='bottom')
+    elif i in [4]:
+        ax.text(x[i]+3, y[i]-0.15, label, fontsize=32, ha='left', va='top')
+    elif i in [3]:
+        ax.text(x[i]+3, y[i]-0.1, label, fontsize=32, ha='left', va='top')
+    elif i in [2]:
+        ax.text(x[i]+3, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    elif i in [1]:
+        ax.text(x[i]+2, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    # else:
+    #     ax.text(x[i]+7, y[i], label, fontsize=32, ha='left', va='top')
+
+# set x-axis to log scale
+# plt.xscale('log')
+plt.ylim(24.5, 29.5)
+plt.yticks([25, 26, 27, 28])
+plt.xticks([0, 10, 20, 30, 40, 50])
+plt.xlim(-2, 68)
+
+# plt.title('Scatter Plot with Labels')
+plt.xlabel(r'$\mathrm{GPU~Memory~(GB)}$', fontsize=40)
+plt.ylabel(r'$\mathrm{Error~\%}$', fontsize=40)
+ax.tick_params(labelsize=40)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig("./figures/plot_memory_vs_performance_mpnn.pdf", format="pdf", dpi=1200)
+plt.show()
---
+++ graph-algorithms/notebooks/plot_runtime_vs_performance.py
@@ -0,0 +1,148 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+from matplotlib import rc
+rc('font', **{'family':'sans-serif','sans-serif':['Helvetica']})
+mpl.rcParams['savefig.dpi'] = 1200
+mpl.rcParams['text.usetex'] = True  # not really needed
+
+
+# Sample data points
+x = [
+30.476,
+19.67087942, 
+107.4831816	,
+220.4783212	,
+148.5070977 ,
+77.16741243	,
+]
+
+y = [
+86.0,
+83.6,
+84.9,
+85.0,
+86.4,
+87.3,
+]
+x = np.array(x) # convert to numpy array
+y = 100 - np.array(y) # compute error rate
+
+labels = [
+          r'$\mathrm{STN}$', 
+          r'$\mathrm{MTN}$', 
+          r'$\mathrm{MMoE}$', 
+          r'$\mathrm{TAG}$',
+          r'$\mathrm{LearningToBranch}$',
+          r'$\mathrm{AutoBRANE}$',]
+
+# Create scatter plot
+f, ax = plt.subplots(figsize=(8, 6))
+ax.scatter(x[1:-1], y[1:-1], color='orange', s=600)
+ax.scatter(x[-1], y[-1], color='forestgreen', s=600, marker='D')
+# Label each point
+for i, label in enumerate(labels):
+    if i in [5]:
+        ax.text(x[i]+14, y[i]-0.3, label, fontsize=32, ha='left', va='bottom')
+    elif i in [4]:
+        ax.text(x[i]-7, y[i]+0.2, label, fontsize=32, ha='left', va='bottom')
+    elif i in [3]:
+        ax.text(x[i]+12, y[i]+0.1, label, fontsize=32, ha='left', va='top')
+    elif i in [2]:
+        ax.text(x[i]+10, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    elif i in [1]:
+        ax.text(x[i]+6, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+
+# set x-axis to log scale
+# plt.xscale('log')
+plt.ylim(12.3, 17.5)
+plt.yticks([13, 14, 15, 16, 17])
+plt.xticks([0, 50, 100, 150, 200, 250])
+plt.xlim(-10, 310)
+
+# plt.title('Scatter Plot with Labels')
+plt.xlabel(r'$\mathrm{GPU~Hours}$', fontsize=40)
+plt.ylabel(r'$\mathrm{Error~\%}$', fontsize=40)
+ax.tick_params(labelsize=40)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig("./figures/plot_runtime_vs_performance.pdf", format="pdf", dpi=1200)
+plt.show()
+
+# %%
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib as mpl
+
+from matplotlib import rc
+rc('font', **{'family':'sans-serif','sans-serif':['Helvetica']})
+mpl.rcParams['savefig.dpi'] = 1200
+mpl.rcParams['text.usetex'] = True  # not really needed
+
+
+# Sample data points
+x = [
+3.278479903,
+3.33093461,
+33.40580624,	
+14.92821967,
+21.83927907,
+10.86864964	
+]
+
+y = [
+75.2,
+71.6,
+71.7,
+73.1,
+73.7,
+74.6,
+]
+x = np.array(x) # convert to numpy array
+y = 100 - np.array(y) # compute error rate
+
+labels = [
+          r'$\mathrm{STN}$', 
+          r'$\mathrm{MTN}$', 
+          r'$\mathrm{MMoE}$', 
+          r'$\mathrm{TAG}$',
+          r'$\mathrm{LearningToBranch}$',
+          r'$\mathrm{AutoBRANE}$',]
+
+# Create scatter plot
+f, ax = plt.subplots(figsize=(8, 6))
+ax.scatter(x[1:-1], y[1:-1], color='orange', s=600)
+ax.scatter(x[-1], y[-1], color='forestgreen', s=600, marker='D')
+# Label each point
+for i, label in enumerate(labels):
+    if i in [5]:
+        ax.text(x[i]+4, y[i]-0.3, label, fontsize=32, ha='left', va='bottom')
+    elif i in [4]:
+        ax.text(x[i]+3, y[i]-0.3, label, fontsize=32, ha='left', va='bottom')
+    elif i in [3]:
+        ax.text(x[i]+3, y[i]+0.2, label, fontsize=32, ha='left', va='top')
+    elif i in [2]:
+        ax.text(x[i]+3, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+    elif i in [1]:
+        ax.text(x[i]+3, y[i]+0.1, label, fontsize=32, ha='left', va='bottom')
+
+# set x-axis to log scale
+# plt.xscale('log')
+plt.ylim(24.5, 29.5)
+plt.yticks([25, 26, 27, 28])
+plt.xticks([0, 10, 20, 30, 40, 50])
+plt.xlim(-10, 65)
+
+# plt.title('Scatter Plot with Labels')
+plt.xlabel(r'$\mathrm{GPU~Hours}$', fontsize=40)
+plt.ylabel(r'$\mathrm{Error~\%}$', fontsize=40)
+ax.tick_params(labelsize=40)
+plt.grid(True, alpha=0.3)
+plt.tight_layout()
+plt.savefig("./figures/plot_runtime_vs_performance_mpnn.pdf", format="pdf", dpi=1200)
+plt.show()
