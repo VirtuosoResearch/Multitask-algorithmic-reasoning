40c0cf708b7a5289d23c638628b833f5695ddd88
lidongyue12138 <lidongyue12138@gmail.com>
2025-06-01 20:35:10 -0400

    sync

--- graph-algorithms/scripts/train_clrs_text/tune_graph_llama.sh
+++ graph-algorithms/scripts/train_clrs_text/tune_graph_llama.sh
@@ -43,7 +43,7 @@ do
 #     --use_graph_llama --alignment_loss_weight $weight
 
 python train_clrs_text.py --task_names 'dijkstra' \
-        --model_key "meta-llama/Llama-3.1-8B" \
+    --model_key "meta-llama/Llama-3.1-8B" \
     --devices 0 --batch_size 4 --inference_batch_size 4 --max_length 32 --max_output_length 256 --generate_output --runs 1 --lr 5e-5 \
     --save_name tuning_stl --epochs 20 --train_lora --lora_rank 16 --lora_alpha 128 \
     --few_shot_k 0 --downsample_ratio 0.05 --minimum_samples 1000 --minimum_samples_validation 100 --train_lengths 15 --test_lengths 15 --use_graph_llama --alignment_loss_weight $weight
