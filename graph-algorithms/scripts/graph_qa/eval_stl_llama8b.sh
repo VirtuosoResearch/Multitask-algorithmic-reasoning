1e660872c10f55190c6ffc363a1c183c1e77df00
zhenshuozhang <zs.zhang0120@gmail.com>
2025-06-02 18:15:14 -0400

    scripts

--- graph-algorithms/scripts/graph_qa/eval_stl_llama8b.sh
+++ graph-algorithms/scripts/graph_qa/eval_stl_llama8b.sh
@@ -20,3 +20,9 @@ python train.py --task_names $task_name --prompt_styles "zero_shot" --text_encod
 done
 
 done
+
+python train_graphqa.py --task_names 'triangle_counting' --prompt_styles "zero_shot" --text_encoders "incident"\
+    --model_key "meta-llama/Llama-3.1-8B" \
+    --devices 1 --batch_size 8 --inference_batch_size 8 --max_length 512 --max_output_length 64 --generate_output --runs 1 --lr 5e-5 \
+    --train_lora --lora_rank 16 --lora_alpha 128 \
+    --save_name test --epochs 0 --precision "bf16-true"  --write_results --min_nodes 10 --max_nodes 11
